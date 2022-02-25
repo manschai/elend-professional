be44e7ecf51566dd78c3a2b059e3b627c388ba47
Schaible <manfred.schaible@atos.net>
2022-02-25 15:32:33 +0100

    lendit

---
+++ src/main/java/com/pls/lendit/LenditApplication.java
@@ -0,0 +1,13 @@
+package com.pls.lendit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LenditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LenditApplication.class, args);
+	}
+
+}
